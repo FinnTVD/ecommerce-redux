6c1702a344e78d378c108ad036242538146195a2
Văn Đức <finnit.th@gmail.com>
2023-04-12 16:13:18 +0700

    update

--- src/utils/global.js
+++ src/utils/global.js
@@ -52,7 +52,8 @@ export const customArray = (length) => {
 	return new Array(length).fill(0);
 };
 
-export const urlApi = "http://localhost:3000";
+// export const urlApi = "http://localhost:3000";
+export const urlApi = "https://api-ecommerce-redux.vercel.app";
 export const portAuth = "http://localhost:5000";
 export const urlApiShoppingCart = `${urlApi}/shoppingCart/`;
 export const cookie_domain = window.location.hostname;
